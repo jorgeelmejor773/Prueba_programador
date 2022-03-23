c19cfc4d4e02a0afeac1028c4ca7cb85c0cab472
jorgeelmejor773 <99939567+jorgeelmejor773@users.noreply.github.com>
2022-03-22 21:59:50 -0500

    Add files via upload

---
+++ ejercicio2 b/ejercicio2b/src/main/java/operaAritme.java
@@ -0,0 +1,12 @@
+
+
+public class operaAritme {
+    public static void main(String[]args){
+        int a=5, b=9, d=3;
+        String c = "he";
+        boolean z;
+        z= c != c;
+        System.out.println(z);
+        
+    }
+}
